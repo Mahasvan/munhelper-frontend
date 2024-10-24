be32f894185590db5b2800b636e2d481b4e5a834
Mahasvan <mahasvan@gmail.com>
2024-10-24 21:22:40 +0530

    Code formatting

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 import "./sidebar.css"
-import {Context} from '../context/Context'
+import { Context } from '../context/Context'
 
 const Sidebar = () => {
   return (
     <div className='sidebar_container'>
-        <span>Related Documents</span>
-        <Context />
+      <span>Related Documents</span>
+      <Context />
     </div>
   )
 }
